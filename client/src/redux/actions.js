1192c9a6b33f05edfcdfb89549e4c00a46a8e1db
Erik <mortensenerikm@gmail.com>
2023-05-29 15:58:15 -0300

    Se agrega el middleware cors a la api - getUsers funcionando en el front

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -3,7 +3,7 @@ import { GET_USERS } from "./actions-types";
 
 export const getUsers = () => {
     return async function (dispatch) {
-        const users = (await axios.get('https://localhost:3001/videogames')).data;
+        const users = (await axios.get('http://localhost:3001/videogames')).data;
         console.log(users);
         // const users = (await axios.get('https://jsonplaceholder.typicode.com/users')).data;
 
