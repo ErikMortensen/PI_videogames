23f672a683811bea188de2855aa3602fcae85f59
Erik <mortensenerikm@gmail.com>
2023-06-07 16:00:39 -0300

    las cards se ven bastante bien, faltan las estrellas

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,16 +1,16 @@
 import {Link} from "react-router-dom";
-import style from "./Card.module.css";
+import styles from "./Card.module.css";
 
 export const Card = ({id,name,image,rating,genres}) => {
   return (
     <Link to={`/detail/${id}`}>
-      <div className={style.card}>
+      <div className={styles.card}>
+        <h4 className={styles.cardRating}>{rating}</h4>
         <img src={image} alt="imagen videogame" />
-        <h3>Name: {name}</h3>
-        <h3>Rating: {rating}</h3>
-        <h3>Genres: {genres?.map((genre,key) => {
-                    return <h5 key={key}>{genre}</h5>
-                  })}</h3>
+        <h1>{name}</h1>
+          <h3 className={styles.cardGenres}>{genres?.map((genre,key) => {
+            return <h5 key={key}>{genre}</h5>
+          })}</h3>
       </div>
     </Link>
   )
